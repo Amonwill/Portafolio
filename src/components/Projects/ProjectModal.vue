0d80e7fc3ccec9f646ef84ef0ff6c313b63b8b70
Amonwill <wcruzhernandez59@gmail.com>
2026-08-05 18:21:58 -0600

    Feat: Traduccion automatica de EN a ES

--- src/components/Projects/ProjectModal.vue
+++ src/components/Projects/ProjectModal.vue
@@ -18,12 +18,12 @@
         
         <div class="left-col">
           <div class="info-section">
-            <h3>Sobre el proyecto</h3>
+            <h3>About the Project</h3>
             <p>{{ project.detailedDesc || project.desc }}</p>
           </div>
 
           <div class="info-section">
-            <h3>Tecnologías y Frameworks</h3>
+            <h3>Technologies and Frameworks</h3>
             <div class="tech-chips-container">
               <div v-for="t in project.tech" :key="t.name" class="tech-chip">
                 <img v-if="t.iconType === 'Imagen'" :src="urlFor(t.image).url()" class="chip-icon-img" />
@@ -36,10 +36,10 @@
 
           <div class="modal-links">
             <a v-if="project.github" :href="project.github" target="_blank" class="cv-btn cv-btn-primary">
-              <i class="fab fa-github"></i> Código
+              <i class="fab fa-github"></i> Project Code
             </a>
             <a v-if="project.liveUrl" :href="project.liveUrl" target="_blank" class="cv-btn cv-btn-secondary">
-              <i class="fas fa-external-link-alt"></i> Demo Live
+              <i class="fas fa-external-link-alt"></i> Live Project
             </a>
           </div>
         </div>
@@ -47,11 +47,11 @@
         <div class="right-col">
           <div class="image-stack">
             <div class="image-wrapper">
-              <img :src="urlFor(project.image).url()" class="modal-main-img" alt="Portada" />
+              <img :src="urlFor(project.image).url()" class="modal-main-img" alt="cover image" />
             </div>
             
             <div v-for="(img, idx) in project.gallery" :key="idx" class="image-wrapper">
-              <img :src="urlFor(img).url()" class="modal-main-img" alt="Captura de pantalla" />
+              <img :src="urlFor(img).url()" class="modal-main-img" alt="screen capture" />
             </div>
           </div>
         </div>
