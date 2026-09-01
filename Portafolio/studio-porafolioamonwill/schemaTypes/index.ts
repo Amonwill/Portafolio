307e4674e0bee7b5795e4c977761ada27cb1df6c
Amonwill <wcruzhernandez59@gmail.com>
2026-09-01 02:49:24 -0600

    new svg icon

--- Portafolio/studio-porafolioamonwill/schemaTypes/index.ts
+++
@@ -1,6 +0,0 @@
-import projects from './projects'
-import Certification from './Certification'
-import habilidades from './habilidades'
-import author from './author'
-
-export const schemaTypes = [author, projects, Certification, habilidades]
